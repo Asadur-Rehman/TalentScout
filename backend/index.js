9da8b50151f13bcb79f523a3bdaba5c37ddafffe
AsadurRehman <aurehman.bese21seecs@seecs.edu.pk>
2025-02-12 08:11:29 +0500

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import recruiterRouter from "./routes/recruiter.route.js";
 import authRouter from "./routes/auth.route.js";
 import jobRouter from "./routes/job.route.js";
+import candidateRouter from "./routes/candidate.route.js";
+import interviewRouter from "./routes/interview.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -14,7 +16,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => console.log(err));
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -28,12 +30,14 @@ app.listen(3000, () => {
 app.use("/api/recruiter", recruiterRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/job", jobRouter);
+app.use("/api/candidate", candidateRouter);
+app.use("/api/interview", interviewRouter);
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
