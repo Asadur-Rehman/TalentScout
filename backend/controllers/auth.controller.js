144e10120d750462980145b53a85a19c0569a97b
AsadurRehman <aurehman.bese21seecs@seecs.edu.pk>
2025-02-19 00:58:23 +0500

    Added Organization name in sign up functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,10 +4,11 @@ import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, organizationname, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newRecruiter = new Recruiter({
     username,
+    organizationname,
     email,
     password: hashedPassword,
   });
@@ -42,11 +43,14 @@ export const signin = async (req, res, next) => {
 
 export const google = async (req, res, next) => {
   try {
-    const recruiter = await Recruiter.findOne({ email: req.body.email });
+    const { email, username, organizationname } = req.body; // Include organizationname
+
+    let recruiter = await Recruiter.findOne({ email });
+
     if (recruiter) {
       const token = jwt.sign({ id: recruiter._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = recruiter._doc;
-      res
+      return res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json(rest);
@@ -55,16 +59,21 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
       const newRecruiter = new Recruiter({
         username:
-          req.body.username.split(" ").join("").toLowerCase() +
+          username.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-4),
-        email: req.body.email,
+        email,
         password: hashedPassword,
+        organizationname, // Store the organization name
       });
+
       await newRecruiter.save();
+
       const token = jwt.sign({ id: newRecruiter._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newRecruiter._doc;
+
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
--- backend/routes/recruiter.route.js
+++ backend/routes/recruiter.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 //import { deleteRecruiter, test, updateRecruiter,  getRecruiterJobs, getRecruiter} from '../controllers/user.controller.js';
 import { updateRecruiter } from "../controllers/recruiter.controller.js";
+import { checkRecruiter } from "../controllers/recruiter.controller.js";
 import { test } from "../controllers/recruiter.controller.js";
 import { verifyToken } from "../utils/verifyRecruiter.js";
 
@@ -8,8 +9,9 @@ const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateRecruiter);
+router.post("/check/", checkRecruiter);
 // router.delete('/delete/:id', verifyToken, deleteUser)
 // router.get('/listings/:id', verifyToken, getUserListings)
-// router.get('/:id', verifyToken, getUser)
+// router.get("/:id", verifyToken, getRecruiter);
 
 export default router;
